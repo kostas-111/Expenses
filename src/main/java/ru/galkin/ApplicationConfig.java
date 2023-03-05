333f0bb0c76b1533c800dd07de7458f305addfd9
kostas-111 <galkink370@gmail.com>
2023-03-05 14:44:44 +0300

    Start lesson

---
+++ src/main/java/ru/galkin/ApplicationConfig.java
@@ -0,0 +1,10 @@
+package ru.galkin;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+@FacesConfig
+@ApplicationScoped
+public class ApplicationConfig {
+
+}
